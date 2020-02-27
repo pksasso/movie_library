f46bda5e5bc75807e90986a4dafad3d018b2d6df
Patrick Sasso <patricksassorj@gmail.com>
2020-02-27 19:39:27 -0300

    *Bug fix when reload genre page

--- src/containers/Genre.js
+++ src/containers/Genre.js
@@ -2,36 +2,39 @@ import React, { useEffect, useContext } from 'react';
 import { animateScroll } from 'react-scroll';
 
 import MovieList from '../components/MovieList';
-import { getMovieByGenre, clearMovie } from '../connections/connections';
+import { getMovieByGenre, clearMovie, loadGenre } from '../connections/connections';
 
 import { MovieContext } from '../contexts/MovieContext';
 
 function Genre({ match }) {
 
   const {
-    genreList,
-    genreId,
     setMenuSelected,
     movieList,
     setMovieList } = useContext(MovieContext);
 
   const query = match.params.name.replace(/\s+/g, ' ').toLowerCase();
 
+  function fetchGenreId() {
+    console.log('rodei')
+    loadGenre().then(res => {
+      getMovieByGenre(setMovieList,
+        res.data.genres.find(item => item.name.toLowerCase() === query).id
+      )
+    });
+  }
+
   useEffect(() => {
     animateScroll.scrollToTop({ smooth: true });
-    getMovieByGenre(setMovieList,
-      genreList.find(item => item.name.toLowerCase() === query).id
-    );
+    fetchGenreId();
     setMenuSelected(query);
     return () => {
       clearMovie(setMovieList);
     }
   }, [query,
-    genreId,
-    match.params.name,
     setMenuSelected,
-    setMovieList,
-    genreList]);
+    setMovieList
+  ]);
 
   return (
     <>

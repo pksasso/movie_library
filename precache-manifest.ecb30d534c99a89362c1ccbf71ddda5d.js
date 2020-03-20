d1ee442350fc3b5716a8db4bf2a822764bae6fa5
pksasso <patricksassorj@gmail.com>
2020-03-20 19:24:55 -0300

    Updates

---
+++ precache-manifest.ecb30d534c99a89362c1ccbf71ddda5d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "521fc0aaa57fb1be729be3288b4fd638",
+    "url": "/movie_library/index.html"
+  },
+  {
+    "revision": "a72af85f3181b100dc7c",
+    "url": "/movie_library/static/css/main.491a9096.chunk.css"
+  },
+  {
+    "revision": "9690f99e641f39a1a851",
+    "url": "/movie_library/static/js/2.3794833f.chunk.js"
+  },
+  {
+    "revision": "280b48dbc3281f342da16f66e41cf46c",
+    "url": "/movie_library/static/js/2.3794833f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a72af85f3181b100dc7c",
+    "url": "/movie_library/static/js/main.e3bb000e.chunk.js"
+  },
+  {
+    "revision": "dda7d6b2475c0a054fa3",
+    "url": "/movie_library/static/js/runtime-main.1bac623d.js"
+  }
+]);
